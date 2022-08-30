448f0037c703a5c7a5406fc39d6b9811b12ebfab
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2022-08-30 12:15:37 +0200

    js all

--- src/index.js
+++ src/index.js
@@ -1,103 +1,90 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let changeDate = document.querySelector("#date-change");
-changeDate.innerHTML = `Today is ${day} ${hours}:${minutes}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = 0${hours};
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = 0${minutes};
+  }
 
-currentTemp.innerHTML = celsiusTemperature;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return ${day} ${hours}:${minutes};
+}
 
-function changeFahrenheit(event) {
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#currentTemp");
+  let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date-change");
+
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "c664d12fba35f3eac42867976a8ddf54";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  let fahrenheit = document.querySelector("#currentTemp");
-  let fahrenheitLink = document.querySelector("#fahrenheitLink");
-  let celsiusLink = document.querySelector("#celsiusLink");
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  fahrenheit.innerHTML = Math.round(fahrenheitTemperature);
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
-function changeCelsius(event) {
+function displayCelsiusTemperature(event) {
   event.preventDefault();
-  let celsius = document.querySelector("#currentTemp");
-  let celsiusLink = document.querySelector("#celsiusLink");
-  let fahrenheitLink = document.querySelector("#fahrenheitLink");
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
-  celsius.innerHTML = Math.round(celsiusTemperature);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
-function displayWeather(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#currentTemp").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}`;
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind-speed").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-}
-
-function retrieveCityInfo(event) {
-  event.preventDefault();
-  let apiKey = "c664d12fba35f3eac42867976a8ddf54";
-  let city = document.querySelector("#city-input").value;
-  let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndPoint}?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", retrieveCityInfo);
-
-function showLocation(position) {
-  let apiKey = "c664d12fba35f3eac42867976a8ddf54";
-  let long = position.coords.longitude;
-  let lat = position.coords.latitude;
-  let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-}
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showLocation);
-}
-
-let currentButton = document.querySelector("#current-location-button");
-currentButton.addEventListener("click", getCurrentPosition);
-
-let fahrenheit = document.querySelector("#fahrenheitLink");
-let celsius = document.querySelector("#celsiusLink");
-fahrenheit.addEventListener("click", changeFahrenheit);
-celsius.addEventListener("click", changeCelsius);
-
-let currentCity = document.querySelector("#currentLocation");
-currentCity.addEventListener("click", currentLocation);
-
-let formInput = document.querySelector("#search-form");
-formInput.addEventListener("submit", getWeatherFromSearchForm);
-
-let defaultCity = "Lozova";
-getWeatherByCity(defaultCity);
 
 let celsiusTemperature = null;
 
-let units = "imperial";
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-searchCity("Lozova");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Lozova");
